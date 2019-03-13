80e0bd3eb1d114a030e2043231763d561072cd61
Andy <andriy.shevtsiv@gmail.com>
2019-03-13 17:13:49 +0000

    edited-19-03-13

--- js/product-list.js
+++ js/product-list.js
@@ -27,7 +27,7 @@ class ProductList {
                         data-target="#productInfoModal" data-id="${product.id}">Info
                       </button>
                       <button class="btn btn-primary buy" data-id="${product.id}">
-                        $${product.price} - Buy
+                        ₴${product.price} - Buy
                       </button>
                     </div>
                   </div>
@@ -47,7 +47,7 @@ class ProductList {
             modal.find('.modal-body .card-title').text(product.title);
             modal.find('.modal-body .card-text').text(product.description);
             modal.find('button.buy')
-                .text(`${product.price} - Buy`)
+                .text(`₴${product.price} - Buy`)
                 .data('id', id);
         });
         $('.card.product button.buy, #productInfoModal button.buy').click( event => {
